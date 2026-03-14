5426886f7eb3becacafc2cc04b1a0dbcf67dc9ca
Poohbase <poohbase@gmail.com>
2026-03-14 22:09:03 +0700

    edit_from

--- my-app/src/components/User.jsx
+++ my-app/src/components/User.jsx
@@ -1,21 +1,30 @@
 import boy from "../assets/boy.svg";
 import girl from "../assets/girl.svg";
+import { FaTrash } from "react-icons/fa";
 
-export default function User({item,deleteUser}) {
+export default function User({item, deleteUser}) {
+  const isMale = item.gender === "ชาย";
+
   return (
-    <>
-      <li
-        style={{
-          borderStyle: "solid",
-          borderColor: item.gender == "ชาย" ? "green" : "pink",
-        }}
-      >
-        <img src={item.gender == "ชาย" ? boy : girl} width={50} height={50} />
-        <p>{item.name}</p>
-        <div className="control">
-          <button onClick={()=>deleteUser(item.id)}>ลบ</button>
+    <li className={`user-card ${isMale ? 'male' : 'female'}`}>
+      <div className="user-info">
+        <div className="avatar-wrapper">
+          <img src={isMale ? boy : girl} alt={item.gender} className="avatar" />
         </div>
-      </li>
-    </>
+        <div className="details">
+          <h3 className="user-name">{item.name}</h3>
+          <span className="gender-badge">{item.gender}</span>
+        </div>
+      </div>
+      <div className="control">
+        <button 
+          className="delete-btn" 
+          onClick={() => deleteUser(item.id)} 
+          title="ลบข้อมูล"
+        >
+          <FaTrash />
+        </button>
+      </div>
+    </li>
   );
 }
